Fixed direction normalization, started encoder for dynamic graph

src/main.py
@@ -9,6 +9,17 @@ from step6_gcn_gru_combined_model import *
 from step4_sequence_preparer import *
 from step7_DQN import *
 
+import pandas as pd
+
+#######################################
+# TO-DO (Aug 10, 2023)
+# Finish unsupervised clustering as per https://towardsdatascience.com/deep-clustering-with-sparse-data-b2eb1bf2922e
+# Left off by preprocessing attribute matrix to construct dynamic adjacency matrix: 
+#   made 26301 dataframes (one for each hour-time-step) of a 34x16 matrix (stations x attributes)
+# Need to code the encoder/manifold cluster algorithm
+#######################################
+
+
 if __name__ == "__main__":
     
     # Path to save best model to
@@ -17,6 +28,19 @@ if __name__ == "__main__":
     # Using GPU if available
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     torch.cuda.empty_cache()
+
+    t1 = [[[2,3],[4,5]],[[7,8],[9,1]],[[1,6],[5,2]]]
+    tt1 = torch.tensor(t1)
+    t2 = [[[1,6],[2,1]],[[1,4],[3,1]],[[1,7],[9,1]]]
+    tt2 = torch.tensor(t2)
+    t3 = [[1,0],[0,1]]
+    tt3 = torch.tensor(t3)
+
+    tOut1 = torch.matmul(tt3,tt2).numpy()
+    print(tOut1)
+    print("break")
+    tOut2 = torch.matmul(tt1,tt2).numpy()
+    print(tOut2)
     # Step 1a - Load data from both csv (measurements and coordinates)
     # Step 1b - Preprocess data
     
@@ -24,7 +48,6 @@ if __name__ == "__main__":
     large = True
     df, wind_min, wind_max = load_and_process_wind_speed_dataset(dataset_size=large)
     
-
     # Step 2 - Build distance graph
     root_graph = build_A_hat(df)
     temp_graph = root_graph
@@ -32,9 +55,14 @@ if __name__ == "__main__":
     adj_matrix = torch.tensor(adj_matrix).float()
     adj_matrix = adj_matrix.to(device)
 
-    # Step 3 - Select important features
-    attr_matrix = extract_features(df)
+    # Step 3 - Select important features and also return attributes for each time step
+    attr_matrix, attr_frames = extract_features(df)
+    # print(attr_matrix)
+    attr_matrix = attr_matrix.to_numpy()
 
+    # Step 3b - Use selected features to build adjacency matrix for each time step
+    
+    
     # Step 4 - generate train / test data sequences
     stations = np.unique(attr_matrix[:, 0])
     num_stations = len(stations)
@@ -52,7 +80,7 @@ if __name__ == "__main__":
     actions = createActions()
 
     learning_rate = 0.001
-    epochs = 700  # number of times the model sees the complete dataset
+    epochs = 500  # number of times the model sees the complete dataset
 
     # Defining Loss Function
     lossFunction = nn.MSELoss()  # Mean Squared Error - default for regression problems
@@ -129,7 +157,7 @@ if __name__ == "__main__":
 
         print("epoch: %d, patience: %d, val loss: %1.5f test loss: %1.5f" % (epoch, patience, val_loss, best_loss))
 
-        if patience > 50:
+        if patience > 40:
             break
         
         # if the model has improved beyond the threshold, start fine tuning with reinforcement learning
@@ -166,8 +194,10 @@ if __name__ == "__main__":
     with torch.no_grad():
         for (batch_x, batch_y) in test_loader:
             outputs = model(adj_matrix, batch_x)
-            test_out_inv_norm = outputs.detach().cpu().numpy() * (wind_max - wind_min) + wind_min
-            test_lab_inv_norm = batch_y.detach().cpu().numpy() * (wind_max - wind_min) + wind_min + [1]
+            test_out_inv_norm = (outputs.detach().cpu().numpy()) * (wind_max - wind_min) + wind_min
+            test_lab_inv_norm = (batch_y.detach().cpu().numpy()) * (wind_max - wind_min) + wind_min
+            # test_out_inv_norm = outputs.detach().cpu().numpy() * (wind_max + wind_min) * 2 + (wind_max + wind_min) / 2
+            # test_lab_inv_norm = batch_y.detach().cpu().numpy() * (wind_max + wind_min) * 2 + (wind_max + wind_min) / 2
             test_diff = abs(test_lab_inv_norm[0] - test_out_inv_norm)
             test_loss_list.append(test_diff)
             predictions.append(test_out_inv_norm)

src/step1_loading_preprocessing.py
@@ -64,14 +64,24 @@ def load_and_process_wind_speed_dataset(verbose: bool = True, dataset_size: bool
     wind_data_min = torch.min(wind_data, dim=0, keepdim=True)[0].numpy()
     wind_data_max = torch.max(wind_data, dim=0, keepdim=True)[0].numpy()
 
-    
-
     numerical_data = torch.tensor(full_df[numerical_features].values)
     numerical_data_min = torch.min(numerical_data, dim=0, keepdim=True)[0]
     numerical_data_max = torch.max(numerical_data, dim=0, keepdim=True)[0]
-    # normalized_numerical_data = (numerical_data - numerical_data_min) / (numerical_data_max - numerical_data_min)
-    normalized_numerical_data = (numerical_data - (numerical_data_min + numerical_data_max)/2) / (numerical_data_max - numerical_data_min) / 2
+    normalized_numerical_data = (numerical_data - numerical_data_min) / (numerical_data_max - numerical_data_min) # from 0 to 1
+    # normalized_numerical_data = (numerical_data - (numerical_data_min + numerical_data_max)/2) / (numerical_data_max - numerical_data_min) / 2 # from -1 to 1
+
+    # normalized_numerical_data[:,11] = (normalized_numerical_data[:,11] - torch.min(normalized_numerical_data[:,11])) / (torch.max(normalized_numerical_data[:,11]) - torch.min(normalized_numerical_data[:,11]))
+    # normalized_numerical_data[:,13] = (normalized_numerical_data[:,13] - torch.min(normalized_numerical_data[:,13])) / (torch.max(normalized_numerical_data[:,13]) - torch.min(normalized_numerical_data[:,13]))
+    
+    normalized_numerical_data[:,12] = ( normalized_numerical_data[:,12] - (torch.min(normalized_numerical_data[:,12]) + torch.max(normalized_numerical_data[:,12])) / 2 ) / (torch.max(normalized_numerical_data[:,12]) - torch.min(normalized_numerical_data[:,12])) / 2
+    normalized_numerical_data[:,14] = ( normalized_numerical_data[:,14] - (torch.min(normalized_numerical_data[:,14]) + torch.max(normalized_numerical_data[:,14])) / 2 ) / (torch.max(normalized_numerical_data[:,14]) - torch.min(normalized_numerical_data[:,14])) / 2
+    normalized_numerical_data[:,16] = ( normalized_numerical_data[:,16] - (torch.min(normalized_numerical_data[:,16]) + torch.max(normalized_numerical_data[:,16])) / 2 ) / (torch.max(normalized_numerical_data[:,16]) - torch.min(normalized_numerical_data[:,16])) / 2
+    normalized_numerical_data[:,17] = ( normalized_numerical_data[:,17] - (torch.min(normalized_numerical_data[:,17]) + torch.max(normalized_numerical_data[:,17])) / 2 ) / (torch.max(normalized_numerical_data[:,17]) - torch.min(normalized_numerical_data[:,17])) / 2
+
+    test = normalized_numerical_data.numpy()
     full_df[numerical_features] = normalized_numerical_data.numpy()
+    speed1 = normalized_numerical_data.select(1,11)
+    speed2 = normalized_numerical_data[:][13]
 
     if verbose:
         print("Loaded and preprocessed data. Shape:", full_df.shape)

src/step4_sequence_preparer.py
@@ -21,7 +21,7 @@ def __create_sequences(data, seq_length):
     xs1 = np.array(xs)
     ys1 = np.array(ys)
     indices = np.arange(xs1.shape[0])
-    np.random.shuffle(indices)
+    #np.random.shuffle(indices)
     xs1 = xs1[indices]
     ys1 = ys1[indices]
     return xs1, ys1
@@ -85,13 +85,13 @@ def generate_sequences(df, batch_size, device):
 
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                                batch_size=1,
-                                               shuffle=True)
+                                               shuffle=False)
     val_loader = torch.utils.data.DataLoader(dataset=validate_dataset,
                                              batch_size=1,
-                                             shuffle=True)
+                                             shuffle=False)
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                               batch_size=1,
-                                              shuffle=True)
+                                              shuffle=False)
     
 
     train_loader2 = torch.utils.data.DataLoader(dataset=train_dataset,